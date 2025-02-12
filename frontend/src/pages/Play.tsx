9b7c9c9290acda6f14623e24455c5a8177f090bd
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-02-12 07:32:30 +0530

    refactor

--- frontend/src/pages/Play.tsx
+++ frontend/src/pages/Play.tsx
@@ -1,57 +1,58 @@
 import { useState } from "react";
 import useWebSocketStore from "../store/socketStore"; // Import the Zustand store
-import {useParams} from "react-router";
+import { useParams } from "react-router";
 import useChessStore from "../store/gameStore.ts";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import ResultModal from "../components/ResultModal.tsx";
 
 export default function Play() {
-const params = useParams();
-const {connect} = useWebSocketStore();
-        const [move, setMove] = useState("");
-        const { sendMessage} = useWebSocketStore();
-        const {player1username, player2username, gameID, result} = useChessStore();
+  const params = useParams();
+  const { connect } = useWebSocketStore();
+  const [move, setMove] = useState("");
+  const { sendMessage } = useWebSocketStore();
+  const { player1username, player2username, gameID, result } = useChessStore();
 
-    //     useEffect(() => {
-    //     connect(); // Connect WebSocket on mount
-    //
-    //     return () => close(); // Disconnect on unmount
-    // }, [connect, close, sendMessage]);
+  //     useEffect(() => {
+  //     connect(); // Connect WebSocket on mount
+  //
+  //     return () => close(); // Disconnect on unmount
+  // }, [connect, close, sendMessage]);
 
-    useEffect(() => {
-        connect(); // Ensure WebSocket stays connected
-    }, [connect]);
+  useEffect(() => {
+    connect(); // Ensure WebSocket stays connected
+  }, [connect]);
 
-        if(!params.gameID) return <div>Bad Request</div>
+  if (!params.gameID) return <div>Bad Request</div>;
 
-        const handleSendMessage = () => {
-        sendMessage({ type: "move", payload: {MoveStr: move, GameID: params.gameID!} });
-        setMove("");
-    };
+  const handleSendMessage = () => {
+    sendMessage({
+      type: "move",
+      payload: { MoveStr: move, GameID: Number(params.gameID!) },
+    });
+    setMove("");
+  };
 
-
-
-        return (
-        <div>
-            {result && <ResultModal/>}
-            <h2>Play</h2>
-
-
-            <div>White: {player1username} Black: {player2username}</div>
-            <div>Game ID: {gameID}</div>
-
-            Move:
-            <input
-                type="text"
-                className="w-full px-3 py-2 border rounded"
-                value={move}
-                onChange={(e) => setMove(e.target.value)}
-            />
-
-
-            <button onClick={handleSendMessage} className="rounded-md border mt-4 p-4  ">Send</button>
-
-
-        </div>
-        );
+  return (
+    <div>
+      {result && <ResultModal />}
+      <h2>Play</h2>
+      <div>
+        White: {player1username} Black: {player2username}
+      </div>
+      <div>Game ID: {gameID}</div>
+      Move:
+      <input
+        type="text"
+        className="w-full px-3 py-2 border rounded"
+        value={move}
+        onChange={(e) => setMove(e.target.value)}
+      />
+      <button
+        onClick={handleSendMessage}
+        className="rounded-md border mt-4 p-4  "
+      >
+        Send
+      </button>
+    </div>
+  );
 }
--- frontend/src/store/socketStore.ts
+++ frontend/src/store/socketStore.ts
@@ -2,74 +2,76 @@ import { create } from "zustand";
 import useChessStore from "./gameStore.ts";
 
 interface Move {
-    MoveStr: string;
-    GameID: string
+  MoveStr: string;
+  GameID: string;
 }
 
 // Define WebSocket message type
 interface Message {
-    type: string;
-    payload?: any;
+  type: string;
+  payload?: any;
 }
 
 // Zustand store type definition
 interface WebSocketState {
-    connect: () => void;
-    sendMessage: (message: Message) => void;
-    close: () => void;
+  connect: () => void;
+  sendMessage: (message: Message) => void;
+  close: () => void;
 }
 
 // Create Zustand store
 const useWebSocketStore = create<WebSocketState>((set, get) => {
-    let socket: WebSocket | null = null;
+  let socket: WebSocket | null = null;
 
-    return {
-        connect: () => {
-            if (socket && socket.readyState === WebSocket.OPEN) return;
+  return {
+    connect: () => {
+      if (socket && socket.readyState === WebSocket.OPEN) return;
 
-            socket = new WebSocket("ws://localhost:8080/ws");
+      socket = new WebSocket("ws://localhost:8080/ws");
 
-            socket.onopen = () => console.log("Connected to WebSocket");
-            socket.onclose = () => console.log("Disconnected from WebSocket");
-            socket.onerror = (error) => console.error("WebSocket Error:", error);
-            socket.onmessage = (e) => {
-                const data = JSON.parse(e.data);
-                if (data.type === "move") {
-                    useChessStore.getState().makeMove(data.move);
-                }
-                if (data.type === "Init_Game") {
-                    console.log(data);
-                    useChessStore.setState((state) => ({
-                        gameID: data.payload.GameID,
-                        player1username: data.payload.player1username,
-                        player2username: data.payload.player2username,
-                    }));
-                }
-                if(data.type==="Result_Alert"){
+      socket.onopen = () => console.log("Connected to WebSocket");
+      socket.onclose = () => console.log("Disconnected from WebSocket");
+      socket.onerror = (error) => console.error("WebSocket Error:", error);
+      socket.onmessage = (e) => {
+        const data = JSON.parse(e.data);
+        if (data.type === "move") {
+          useChessStore.getState().makeMove(data.move);
+        }
+        if (data.type === "Init_Game") {
+          console.log(data);
+          useChessStore.setState((state) => ({
+            gameID: data.payload.GameID,
+            player1username: data.payload.player1username,
+            player2username: data.payload.player2username,
+          }));
+        }
+        if (data.type === "Result_Alert") {
+          console.log(data);
+          if (data.payload.Result === "1-0")
+            useChessStore.setState((state) => ({ result: "1-0" }));
+          else if (data.payload.Result === "0-1")
+            useChessStore.setState((state) => ({ result: "0-1" }));
+          else if (data.payload.Result === "1/2-1/2")
+            useChessStore.setState((state) => ({ result: "1/2-1/2" }));
+        }
+      };
+    },
 
-                    if(data.payload.Result === "1-0")useChessStore.setState((state) => ({result: "1-0"}))
-                    else if(data.payload.Result === "0-1") useChessStore.setState((state) => ({result: "0-1"}))
-                    else if (data.payload.Result === "1/2-1/2")useChessStore.setState((state) => ({result: "1/2-1/2"}))
-                }
-            };
-        },
+    sendMessage: (message: Message) => {
+      if (socket && socket.readyState === WebSocket.OPEN) {
+        socket.send(JSON.stringify(message));
+      } else {
+        console.warn("WebSocket is not connected yet.");
+      }
+    },
 
-        sendMessage: (message: Message) => {
-            if (socket && socket.readyState === WebSocket.OPEN) {
-                socket.send(JSON.stringify(message));
-            } else {
-                console.warn("WebSocket is not connected yet.");
-            }
-        },
-
-        close: () => {
-            if (socket) {
-                socket.close();
-                socket = null;
-            }
-        },
-    };
+    close: () => {
+      if (socket) {
+        socket.close();
+        socket = null;
+      }
+    },
+  };
 });
-
 
 export default useWebSocketStore;
