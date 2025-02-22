7e032fed65cace00ea14edf9892f2810c6220f62
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-02-23 00:31:21 +0530

    style play page, add gap to time controls

--- frontend/src/pages/Play.tsx
+++ frontend/src/pages/Play.tsx
@@ -30,7 +30,7 @@ export default function Play() {
   // }, [user]);
   //
   return (
-    <div className="w-full min-h-screen flex items-center justify-center">
+    <div className="w-full h-full flex gap-5 items-center justify-center">
       <button
         onClick={() => handleInitGame(2)}
         className="w-[150px] h-[100px] bg-gray-400 cursor-pointer text-xl"
