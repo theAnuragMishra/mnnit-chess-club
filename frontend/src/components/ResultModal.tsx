0c8efdaa835c916deedb6cf4d252cd6d4c9f7cf2
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-03-15 11:36:36 +0530

    improve result modal styling

--- frontend/src/components/ResultModal.tsx
+++ frontend/src/components/ResultModal.tsx
@@ -7,10 +7,13 @@ export default function ResultModal({ onClose }: { onClose: () => void }) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg text-center w-80 border border-gray-300 relative">
-        <h2 className="text-2xl font-bold text-gray-800 mb-2">Game Over</h2>
-        <p className="text-lg text-gray-600 mb-4">{result}</p>
-        <p className="text-3xl text-black">{reason}</p>
+      <div className="bg-gray-800  p-6 rounded-lg shadow-lg text-center w-80 border border-gray-300 relative">
+        <h2 className="text-2xl font-bold text-white mb-2">Game Over</h2>
+        <p className="text-4xl text-white mb-4">{result}</p>
+        <p className="text-xl text-white">{reason}!</p>
+        <p>
+          {result === "1-0" ? "White is victorius" : "Black is victorious"}.
+        </p>
         <button
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
           onClick={onClose}
