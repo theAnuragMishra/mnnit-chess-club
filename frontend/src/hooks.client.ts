7bda342e1594a987742211c3e08900d055d37202
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-04-20 23:29:53 +0530

    wrap socket connection in try catch

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -2,5 +2,9 @@ import { websocketStore } from '$lib/websocket';
 import type { ClientInit } from '@sveltejs/kit';
 
 export const init: ClientInit = async () => {
-	await websocketStore.connect();
+	try {
+		await websocketStore.connect();
+	} catch (e) {
+		console.log(e);
+	}
 };
