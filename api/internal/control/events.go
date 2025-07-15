d8d21eba25a86218e4afa699a3595bfdc364c508
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-07-15 18:14:05 +0530

    join and leave event

--- api/internal/control/events.go
+++ api/internal/control/events.go
@@ -5,13 +5,14 @@ import "github.com/theAnuragMishra/mnnit-chess-club/api/internal/socket"
 type EventHandler func(c *Controller, event socket.Event, client *socket.Client) error
 
 var handlers = map[string]EventHandler{
-	"move":             Move,
-	"chat":             Chat,
-	"draw":             Draw,
-	"resign":           Resign,
-	"init_game":        InitGame,
-	"room_change":      RoomChange,
-	"leave_room":       LeaveRoom,
-	"create_challenge": CreateChallenge,
-	"accept_challenge": AcceptChallenge,
+	"move":                  Move,
+	"chat":                  Chat,
+	"draw":                  Draw,
+	"resign":                Resign,
+	"init_game":             InitGame,
+	"room_change":           RoomChange,
+	"leave_room":            LeaveRoom,
+	"create_challenge":      CreateChallenge,
+	"accept_challenge":      AcceptChallenge,
+	"join_leave_tournament": JoinLeaveTournament,
 }
