76aa64b3c1729554e240aad9c113bd56a40efaa4
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-08-14 17:41:47 +0530

    add a board page for testing

---
+++ frontend/src/routes/board/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Chessboard from '$lib/components/Chessboard.svelte';
+	import type { Api } from 'chessground/api';
+
+	let g: Api | null = $state(null);
+</script>
+
+<div class="flex">
+	<Chessboard boardConfig={{ viewOnly: true }} setGround={(x) => (g = x)} />
+	<div class="flex flex-col">
+		<button
+			class="cursor-pointer bg-blue-500 p-4"
+			onclick={() => {
+				g?.set({ viewOnly: false });
+			}}>toggle viewonly</button
+		>
+		<button
+			class="cursor-pointer bg-blue-500 p-4"
+			onclick={() => {
+				g?.set({ drawable: { visible: !g.state.drawable.visible } });
+			}}>toggle drawable.visible</button
+		>
+	</div>
+</div>
