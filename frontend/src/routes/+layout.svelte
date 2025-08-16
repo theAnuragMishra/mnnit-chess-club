7bd18277c951bc2a7197ade89d49b16b2692c491
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-08-16 18:49:49 +0530

    temporary fix of overflowing coords

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	let { data, children } = $props();
 </script>
 
-<main class="flex min-h-screen flex-col items-center bg-gray-900 text-white">
+<main class="flex min-h-screen flex-col items-center overflow-x-hidden bg-gray-900 text-white">
 	<Navbar {data} />
 	{@render children()}
 </main>
