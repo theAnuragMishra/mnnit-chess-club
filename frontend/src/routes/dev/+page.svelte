c062a0fa37783b0785308b926d14dad4464347ff
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-08-17 16:01:11 +0530

    add a dev page with hld

---
+++ frontend/src/routes/dev/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import hldImage from '$lib/assets/mcc_hld.svg';
+</script>
+
+<h1 class="mb-2 text-xl">High Level Design</h1>
+
+<img src={hldImage} alt="high level design png" class="h-[85vh]" />
