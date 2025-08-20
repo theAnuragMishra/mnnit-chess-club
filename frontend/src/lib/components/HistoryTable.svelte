fb8fc4dc0a8d9710521793f3e468fbe29990e0fe
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-08-20 19:17:04 +0530

    improve moves table styles

--- frontend/src/lib/components/HistoryTable.svelte
+++ frontend/src/lib/components/HistoryTable.svelte
@@ -3,15 +3,63 @@
 	const moveButtons: (HTMLButtonElement | null)[] = $state([]);
 	$effect(() => {
 		const activeButton = moveButtons[activeIndex];
-		activeButton?.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+		activeButton?.scrollIntoView({ behavior: 'smooth', block: 'end' });
 	});
 </script>
 
-<div
-	class="movescontainer relative z-0 h-[40px] w-full bg-gray-800 text-sm md:h-[200px] md:w-[300px] md:px-3 md:text-lg"
->
+<div class="w-full bg-gray-800 text-sm md:w-[300px] md:text-lg">
+	<div class="flex w-full items-center justify-between bg-[#232327] px-5 md:px-2">
+		{#if moveHistory}
+			<div class="flex gap-[10px]">
+				<button
+					aria-label="go to first move"
+					class="flex cursor-pointer items-center justify-center py-1 hover:bg-gray-700"
+					onclick={() => {
+						setActiveIndex(-1);
+					}}
+				>
+					<svg class="h-[16px] md:h-[24px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
+						><path fill="currentColor" d="M2 14V2h2v5.5l5-5v5l5-5v11l-5-5v5l-5-5V14z" /></svg
+					>
+				</button>
+				<button
+					aria-label="go to previous move"
+					class="flex cursor-pointer items-center justify-center py-1 hover:bg-gray-700"
+					onclick={() => {
+						setActiveIndex(activeIndex - 1);
+					}}
+					><svg class="h-[16px] md:h-[24px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
+						><path fill="currentColor" d="M4 14V2h2v5.5l5-5v11l-5-5V14z" /></svg
+					></button
+				>
+			</div>
+			<div class="flex gap-[10px]">
+				<button
+					aria-label="go to next move"
+					class="flex cursor-pointer items-center justify-center py-1 hover:bg-gray-700"
+					onclick={() => {
+						setActiveIndex(activeIndex + 1);
+					}}
+					><svg class="h-[16px] md:h-[24px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
+						><path fill="currentColor" d="M12 2v12h-2V8.5l-5 5v-11l5 5V2z" /></svg
+					></button
+				>
+				<button
+					aria-label="go to latest move"
+					class={`flex cursor-pointer items-center justify-center py-1 hover:bg-gray-700 ${highlightLastArrow && 'animate-pulse bg-blue-700'}`}
+					onclick={() => {
+						setActiveIndex(moveHistory.length - 1);
+					}}
+				>
+					<svg class="h-[16px] md:h-[24px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
+						><path fill="currentColor" d="M14 2v12h-2V8.5l-5 5v-5l-5 5v-11l5 5v-5l5 5V2z" /></svg
+					>
+				</button>
+			</div>
+		{/if}
+	</div>
 	<div
-		class="moves flex w-full items-center justify-center overflow-x-auto md:grid md:h-[150px] md:grid-cols-[1fr_5fr_5fr] md:place-items-center md:content-start md:items-center md:overflow-y-auto md:overflow-x-hidden"
+		class="grid h-[50px] grid-cols-[1fr_5fr_5fr] place-items-center content-center items-center overflow-y-auto px-2 py-2 md:h-[70px] md:overflow-x-hidden"
 	>
 		{#each moveHistory as move, index}
 			{#if index % 2 == 0}
@@ -22,107 +70,13 @@
 				onclick={() => {
 					setActiveIndex(index);
 				}}
-				class={`h-fit w-fit cursor-pointer px-1 py-1.5 md:px-3 ${activeIndex == index ? 'bg-gray-700' : ''}`}
+				class={`h-fit w-fit cursor-pointer px-1 md:px-3 ${activeIndex == index ? 'bg-gray-700' : ''}`}
 			>
 				{move.MoveNotation}
 			</button>
 		{/each}
 	</div>
-	{#if moveHistory}
-		<button
-			aria-label="go to first move"
-			class="first flex cursor-pointer items-center justify-center py-1 hover:bg-gray-700"
-			onclick={() => {
-				setActiveIndex(-1);
-			}}
-		>
-			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"
-				><path fill="currentColor" d="M2 14V2h2v5.5l5-5v5l5-5v11l-5-5v5l-5-5V14z" /></svg
-			>
-		</button>
-		<button
-			aria-label="go to previous move"
-			class="prev flex cursor-pointer items-center justify-center py-1 hover:bg-gray-700"
-			onclick={() => {
-				setActiveIndex(activeIndex - 1);
-			}}
-			><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"
-				><path fill="currentColor" d="M4 14V2h2v5.5l5-5v11l-5-5V14z" /></svg
-			></button
-		>
-		<button
-			aria-label="go to next move"
-			class="next flex cursor-pointer items-center justify-center py-1 hover:bg-gray-700"
-			onclick={() => {
-				setActiveIndex(activeIndex + 1);
-			}}
-			><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"
-				><path fill="currentColor" d="M12 2v12h-2V8.5l-5 5v-11l5 5V2z" /></svg
-			></button
-		>
-		<button
-			aria-label="go to latest move"
-			class={`last flex cursor-pointer items-center justify-center py-1 hover:bg-gray-700 ${highlightLastArrow && 'animate-pulse bg-blue-700'}`}
-			onclick={() => {
-				setActiveIndex(moveHistory.length - 1);
-			}}
-		>
-			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"
-				><path fill="currentColor" d="M14 2v12h-2V8.5l-5 5v-5l-5 5v-11l5 5v-5l5 5V2z" /></svg
-			>
-		</button>
-	{/if}
 </div>
 
 <style>
-	.movescontainer {
-		display: grid;
-		grid-template-columns: auto 4fr auto;
-		grid-template-areas: 'prev moves next';
-	}
-	.moves {
-		grid-area: moves;
-	}
-	.first {
-		grid-area: first;
-		display: none;
-	}
-	.next {
-		grid-area: next;
-		justify-self: end;
-	}
-	.last {
-		grid-area: last;
-		display: none;
-	}
-	.prev {
-		grid-area: prev;
-		justify-self: start;
-	}
-
-	@media (width >=768px) {
-		.movescontainer {
-			gap: 10px;
-			/* grid-template-columns: 1fr 1fr 1fr 1fr; */
-			align-items: center;
-			align-content: start;
-			grid-template-areas:
-				'first prev next last'
-				'moves moves moves moves';
-			padding-top: 2px;
-		}
-		.movescontainer::before {
-			content: '';
-			position: absolute;
-			background-color: rgb(39, 35, 35);
-			width: 100%;
-			height: 18%;
-			top: 0;
-			z-index: -1;
-		}
-		.last,
-		.first {
-			display: flex;
-		}
-	}
 </style>
