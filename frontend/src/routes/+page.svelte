baac7a69e64a8271589f129761ee147ad115956b
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-08-26 23:28:13 +0530

    texts to hyperlinks

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex h-full w-full flex-col items-center justify-center">
 	{#if !data.user}
-		<p>Login to play</p>
+		<a href="/login" class="text-xl text-blue-500">Login to play</a>
 	{:else}
 		<a href="/play" class="cursor-pointer rounded-lg bg-gray-500 p-3 text-4xl"> Play! </a>
 	{/if}
--- frontend/src/routes/leaderboard/+page.svelte
+++ frontend/src/routes/leaderboard/+page.svelte
@@ -10,8 +10,12 @@
 	<h1 class="mb-3 text-center text-2xl">MCC's Top Rated Players</h1>
 	<hr class="mb-2" />
 	{#each data.leaderboard as player, i}
-		<p style={`font-size: ${topSize - 2 * i}px`} class={`flex w-full justify-center gap-2`}>
+		<a
+			href={`/member/${player.Username}`}
+			style={`font-size: ${topSize - 2 * i}px`}
+			class={`flex w-full justify-center gap-2`}
+		>
 			<span>{player.Username}</span><span>{Math.floor(player.Rating)}</span>
-		</p>
+		</a>
 	{/each}
 </div>
