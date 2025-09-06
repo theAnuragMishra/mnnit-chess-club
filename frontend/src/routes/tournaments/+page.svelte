0a3187816e71bc06aca078ae83dfdb6b48c0b7e7
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-09-07 03:32:11 +0530

    better style tournaments page

--- frontend/src/routes/tournaments/+page.svelte
+++ frontend/src/routes/tournaments/+page.svelte
@@ -5,55 +5,69 @@
 <svelte:head>
 	<title>Tournaments - MCC</title>
 </svelte:head>
-<div class="w-full p-2 md:p-10">
-	<h2 class="text-lg md:text-2xl">Live:</h2>
-	<div class="titles">
-		<p class="mb-[5px] flex justify-around text-lg md:text-xl">
-			<span>Name</span><span>Time Control</span><span>Start Time</span><span>Duration (hours)</span>
-		</p>
-		<hr class="mb-4" />
-		{#each data.live as tournament, i}
-			<p class={`flex justify-around text-lg ${i % 2 == 0 ? '' : ''}`}>
-				<span><a href={`/tournament/${tournament.ID}`}>{tournament.Name}</a></span><span
-					>{tournament.BaseTime / 60}+{tournament.Increment}</span
-				><span
-					>{new Date(tournament.StartTime).toLocaleString('en-IN', {
-						year: '2-digit',
-						month: '2-digit',
-						day: '2-digit',
-						hour: '2-digit',
-						minute: '2-digit',
-						hour12: false
-					})}</span
-				><span>{tournament.Duration / 3600}</span>
-			</p>
-		{/each}
-	</div>
-</div>
-<div class="w-full p-2 md:p-10">
-	<h2 class="text-lg md:text-2xl">Scheduled:</h2>
-	<div class="titles">
-		<p class="mb-[5px] flex justify-around text-lg md:text-xl">
-			<span>Name</span><span>Time Control</span><span>Start Time</span><span>Duration (hours)</span>
-		</p>
-		<hr class="mb-4" />
-		{#each data.scheduled as tournament, i}
-			<p class={`flex justify-around text-lg ${i % 2 == 0 ? '' : ''}`}>
-				<span><a href={`/tournament/${tournament.ID}`}>{tournament.Name}</a></span><span
-					>{tournament.BaseTime / 60}+{tournament.Increment}</span
-				><span
-					>{new Date(tournament.StartTime).toLocaleString('en-IN', {
-						year: '2-digit',
-						month: '2-digit',
-						day: '2-digit',
-						hour: '2-digit',
-						minute: '2-digit',
-						hour12: false
-					})}</span
-				><span>{tournament.Duration / 3600}</span>
-			</p>
-		{/each}
-	</div>
+<div class="flex w-full flex-col gap-5 px-5 py-5">
+	{#if data.live.length > 0}
+		<div class="w-full p-2">
+			<h2 class="mb-2 text-center text-lg md:text-2xl">Live Arenas</h2>
+			<div class="titles flex flex-col gap-3">
+				<!-- <p class="mb-[5px] flex justify-around text-sm md:text-xl">
+					<span>Name</span><span>Time Control</span><span>Start Time</span><span
+						>Duration (hours)</span
+					>
+				</p> -->
+
+				{#each data.live as tournament, i}
+					<p
+						class={`flex justify-around py-3 md:text-lg ${i % 2 == 0 ? 'bg-[#212632]' : 'bg-black'}`}
+					>
+						<span><a href={`/tournament/${tournament.ID}`}>{tournament.Name}</a></span><span
+							>{tournament.BaseTime / 60}+{tournament.Increment}</span
+						><span
+							>{new Date(tournament.StartTime).toLocaleString('en-IN', {
+								year: '2-digit',
+								month: 'short',
+								day: '2-digit',
+								hour: '2-digit',
+								minute: '2-digit',
+								hour12: true
+							})}</span
+						><span>{tournament.Duration / 3600} hrs</span>
+					</p>
+				{/each}
+			</div>
+		</div>
+	{/if}
+	{#if data.scheduled.length > 0}
+		<div class="w-full p-2">
+			<h2 class="mb-2 text-center text-lg md:text-2xl">Coming up</h2>
+			<div class="titles flex flex-col gap-3">
+				<!-- <p class="mb-[5px] flex justify-around text-sm md:text-xl">
+					<span>Name</span><span>Time Control</span><span>Start Time</span><span
+						>Duration (hrs)</span
+					>
+				</p> -->
+
+				{#each data.scheduled as tournament, i}
+					<p
+						class={`flex justify-around py-3 md:text-lg ${i % 2 == 0 ? 'bg-[#212632]' : 'bg-black '}`}
+					>
+						<span><a href={`/tournament/${tournament.ID}`}>{tournament.Name}</a></span><span
+							>{tournament.BaseTime / 60}+{tournament.Increment}</span
+						><span
+							>{new Date(tournament.StartTime).toLocaleString('en-IN', {
+								year: '2-digit',
+								month: 'short',
+								day: '2-digit',
+								hour: '2-digit',
+								minute: '2-digit',
+								hour12: true
+							})}</span
+						><span>{tournament.Duration / 3600} hrs</span>
+					</p>
+				{/each}
+			</div>
+		</div>
+	{/if}
 </div>
 
 <!-- <div class="w-full p-2 md:p-10">
